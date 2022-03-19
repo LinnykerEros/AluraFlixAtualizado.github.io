39e6b501831b081800fe20effd182b6487517339
LinnykerEros <54625876+LinnykerEros@users.noreply.github.com>
2022-03-19 11:31:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,72 +1,43 @@
-let filme = ["Yesterday", "A chegada", "Escola de Rock"];
-let res = document.getElementById("res");
-let container = document.getElementById("container1");
+let containerImg = document.getElementById("containerImg");
+let container = document.getElementById("containerPaiDeTodos");
 let caixa;
-
-let capaFilmes = [
-  "https://i.pinimg.com/originals/50/8f/ba/508fba93c5f1b8f1ea1db81dd969e2a0.jpg",
-  "https://www.planocritico.com/wp-content/uploads/2021/01/175-Lista-Os-Melhores-Episodios-de-Vikings-1.jpg",
-  "https://i.pinimg.com/550x/56/a0/be/56a0bec9ff88fbb573f9bf176e487e0f.jpg",
-  "https://i.ytimg.com/vi/aO-ZaF4FJls/maxresdefault.jpg",
-];
-
-let videos = [
-  "https://www.youtube.com/embed/Ro0yIrOKgbM",
-  "https://www.youtube.com/embed/dV6p83HRk4o",
-  "https://www.youtube.com/embed/iSiqmV13Ffk",
-  "https://www.youtube.com/embed/aO-ZaF4FJls",
-];
+let capa1 = {
+  imagem:
+    "https://i.pinimg.com/originals/50/8f/ba/508fba93c5f1b8f1ea1db81dd969e2a0.jpg",
+  videos: "https://www.youtube.com/embed/Ro0yIrOKgbM",
+};
+let capa2 = {
+  imagem:
+    "https://www.planocritico.com/wp-content/uploads/2021/01/175-Lista-Os-Melhores-Episodios-de-Vikings-1.jpg",
+  videos: "https://www.youtube.com/embed/dV6p83HRk4o",
+};
+let capa3 = {
+  imagem:
+    "https://i.pinimg.com/550x/56/a0/be/56a0bec9ff88fbb573f9bf176e487e0f.jpg",
+  videos: "https://www.youtube.com/embed/iSiqmV13Ffk",
+};
+let capa4 = {
+  imagem: "https://i.ytimg.com/vi/aO-ZaF4FJls/maxresdefault.jpg",
+  videos: "https://www.youtube.com/embed/aO-ZaF4FJls",
+};
+let capaFilmes = [capa1, capa2, capa3, capa4];
 
 //COLOCO A CAPA NA TELA PASSANDO A FUNÇÃO DO FRAME NO CLICK
 function filmeCapa() {
-  res.innerHTML = "";
+  containerImg.innerHTML = "";
   for (let i = 0; i < capaFilmes.length; i++) {
-    let video = videos[i];
-    let img = capaFilmes[i];
-    let foto = document.createElement("img"); //criando um elemento IMG
-    foto.setAttribute("src", `${img}`); //atribuindo SRC em IMG passando o vetor
-    foto.setAttribute("id", "idImagem");
-    foto.addEventListener("click", function () {
-      //adicionando um evento click e passando uma função criando o vídeo
-      createiFrame(video, foto);
-    });
-    res.appendChild(foto); // adicionando a tag IMG na DIV
+    containerImg.innerHTML += `<img class="idImagem${i}" src="${capaFilmes[i].imagem}" alt="" onclick= "createiFrame(${i})"/>`;
   }
 }
-
-//CRIANDO O FRAMe
-function createiFrame(video, img) {
-  let iframe;
-  console.log("FUICLICADO");
-  if (!caixa) {
-    // verificando variável global se ela estiver vazia ssomente na primeira vez ela adiciona o video apenas na div caixa
-    caixa = document.createElement("div");
-    caixa.setAttribute("id", "video");
-    container.appendChild(caixa);
-    iframe = document.createElement("iframe");
-    caixa.appendChild(iframe);
-    button = document.createElement("button");
-    button.innerText = "Remover";
-    button.addEventListener("click", function () {
-      let removDiv = document.getElementById("video");
-      res.removeChild(img);
-      container.removeChild(removDiv);
-      caixa = undefined;
-    });
-    caixa.appendChild(button);
-  }
-  console.log(video);
-  iframe = document.getElementsByTagName("iframe")[0];
-  iframe.setAttribute("src", video);
-  iframe.setAttribute("id", "frame");
-}
-//CHAMO A FUNÇÃO DAS IMAGENS ANTES DE ADICIONAR
 filmeCapa();
-//ADICIONANDO NOVAS CAPAS E VIDEOS
-function adicionar() {
+function adicionar(i) {
+  // let capaFilmesImagens = capaFilmes[i].imagem;
   let capa = document.getElementById("capa");
   let linkVideo = document.getElementById("linkVideo");
-
+  let novaCapa = {
+    imagem: capa.value,
+    videos: linkVideo.value,
+  };
   //VERIFICANDO OS INPUTS
   if (capa.value == "" && linkVideo.value == "") {
     alert("[ERROR] Insira a URL");
@@ -78,16 +49,34 @@ function adicionar() {
     capa.value.endsWith(".jpg") == false &&
     capa.value.endsWith(".png") == false
   ) {
-    alert("[ERROR] Insira o endereço da imagem, CORRETO!");
-  } else if (capaFilmes.includes(capa.value)) {
-    alert("URL JA ADICIONADA");
+    alert("[ERROR] Insira o endereço da capa, CORRETO!");
+    // } else if (capa.value.includes(capaFilmesImagens)) {
+    //   alert("URL JA ADICIONADA");
   } else {
-    //ADICIONANDO NO VETOR "CAPAFILMES" e "VIDEOS"
+    //ADICIONANDO NO VETOR "capaFILMES" e "VIDEOS"
     capa.value = "";
     linkVideo.value = "";
-    capaFilmes.push(capa.value);
-    videos.push(linkVideo.value);
+    capaFilmes.push(novaCapa);
   }
-
   filmeCapa();
+}
+function createiFrame(i) {
+  let iframe;
+  console.log("FUICLICADO");
+  if (!caixa) {
+    caixa = document.createElement("div");
+    caixa.setAttribute("id", "video");
+    container.appendChild(caixa);
+  }
+  console.log(video);
+  iframe = document.getElementsByTagName("iframe")[0];
+  caixa.innerHTML = `<iframe id="frame" src="${capaFilmes[i].videos}"></iframe>`;
+  caixa.innerHTML += ` <button onclick="removFrameImg(${i})">Remover</button>`;
+}
+function removFrameImg(i) {
+  let removDiv = document.getElementById("video");
+  let removImg = document.querySelector(`.idImagem${i}`);
+  removImg.style.display = "none";
+  container.removeChild(removDiv);
+  caixa = undefined;
 }
